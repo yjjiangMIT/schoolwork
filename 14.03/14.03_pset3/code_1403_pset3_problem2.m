f5ad8c30c423e36e5cc6608f3cbee38aed47221a
yjjiang <yjjiang@mit.edu>
2015-10-20 19:56:42 -0400

    14.03

--- 14.03/14.03_pset3/code_1403_pset3_problem2.m
+++ 14.03/14.03_pset3/code_1403_pset3_problem2.m
@@ -2,7 +2,10 @@ figure;
 hold on;
 xlim([0, 8]);
 ylim([0, 8]);
-set(gca, 'fontsize', 30);
+ax = gca;
+ax.XTick = [0, 8];
+ax.YTick = [0, 8];
+set(ax, 'fontsize', 30);
 axis('square');
 for i = 0 : 8
     plot([i,i], [i,8], 'color', 'c', 'linewidth', 2);
@@ -12,3 +15,5 @@ for i = 0 : 16
     plot([0,i], [i,0], 'color', 'g', 'linewidth', 2);
 end
 plot(2, 6, 'marker', 'o', 'markerfacecolor', 'r', 'markeredgecolor', 'r', 'markersize', 10);
+plot([0,8], [0,8], 'color', 'b't
+, 'linewidth', 2);
